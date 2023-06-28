0b43ef7635f97973e04c96b9c021b826a2018f89
silas003 <silaskumi4@gmail.com>
2023-06-28 10:49:21 +0100

    'backend'

---
+++ sms/system/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    StudentID=models.PositiveIntegerField()
+    StudentName=models.CharField(max_length=100)
+    Email=models.EmailField()
+    College=models.CharField(max_length=100)
+    POS=models.CharField(max_length=100)
+    YOA=models.DateField()
